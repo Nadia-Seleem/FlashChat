bbe1c541d98a009bc2b1d778596f15913f2a314a
Nadia Seleem <nadiaseleem97@gmail.com>
2020-09-28 11:23:08 +0300

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Nadia Seleem on 11/02/1442 AH.
+//  Copyright © 1442 Nadia Seleem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
